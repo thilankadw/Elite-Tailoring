768c84b71aaa3be7fc391f6459ba49db493aa061
thilankadw <thilankadesigner@gmail.com>
2023-05-28 10:49:47 +0530

    assets folder

--- pages/index.php
+++ pages/index.php
@@ -1,4 +1,46 @@
-<?php include 'navbar.php';?>
+<!DOCTYPE html>
 
-<?php include 'footer.php';?>
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Elite Tailoring</title>
+    <!--<link rel="stylesheet" href="../style.css" />-->
+    
+    <!--Font_Awesome library importing-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+  </head>
+  <body>
+
+    <div>
+      <!--Navigation Bar Start-->
+
+      <?php include 'navbar.php';?>
+
+      <!--Navigation Bar End-->
+    </div>
+
+    <div class="hero-container">
+      <div class="hero-banner-1">
+        <img src="../assets/home/banner-image-3.png" alt="" style="width: 90%;">
+      </div>
+    </div>
+
+    <div>
+      <!--Footer Start-->
+
+      <?php include 'footer.php';?>
+
+      <!--Footer End-->
+    </div>
+
+
+
+    <script src="../script.js"></script>
+
+  </body>
+
+</html>
 
